597884707ac074a94b08689f59e13046f2cff526
nhidinhny <125183883+nhidinhny@users.noreply.github.com>
2026-08-01 17:58:47 +0700

    feat: load tasks from database

--- src/taskRepository.js
+++ src/taskRepository.js
@@ -10,4 +10,62 @@ async function getAllTasks() {
   return result.rows;
 }
 
-export { getAllTasks };
+async function getTaskById(id) {
+  const result = await pool.query(`
+    SELECT *
+    FROM tasks
+    WHERE id = $1
+    ORDER BY id ASC;
+  `,
+    [id]);
+
+  return result.rows[0];
+}
+
+async function createTask(title) {
+  const result = await pool.query(`
+    INSERT INTO tasks (title)
+    VALUES ($1)
+    RETURNING *;
+  `,
+    [title]);
+
+  return result.rows[0];
+}
+
+async function completeTask(id) {
+  const result = await pool.query(`
+    UPDATE tasks
+    SET completed = TRUE
+    WHERE id = $1
+    RETURNING *;
+  `,
+    [id]);
+
+  return result.rows[0];
+}
+
+async function deleteTask(id) {
+  const result = await pool.query(`
+    DELETE FROM tasks
+    WHERE id = $1
+    RETURNING *;
+  `,
+    [id]);
+
+  return result.rows[0];
+}
+
+async function updateTaskTitle(id, newTitle) {
+  const result = await pool.query(`
+    UPDATE tasks    
+    SET title = $2
+    WHERE id = $1
+    RETURNING *;
+  `,
+    [id, newTitle]);
+
+  return result.rows[0];
+}
+
+export { getAllTasks, getTaskById, createTask, completeTask, deleteTask, updateTaskTitle };
